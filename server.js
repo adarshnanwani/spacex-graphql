d795b584cb3063344d74f7a1111e919f90bc5673
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-04-14 21:15:23 +0530

    Add client side with components.

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const cors = require('cors');
 const app = express();
 const schema = require('./schema');
+
+app.use(cors());
 
 app.use(
   '/graphql',
