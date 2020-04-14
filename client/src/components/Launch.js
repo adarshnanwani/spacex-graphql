547396c735d988fdd4df0a370eb907947ac7c2c7
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-04-14 23:57:32 +0530

    Add launch details.

--- client/src/components/Launch.js
+++ client/src/components/Launch.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { withRouter, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Query } from 'react-apollo';
 import { gql } from 'apollo-boost';
 import classNames from 'classnames';
@@ -28,12 +28,54 @@ const Launch = ({ match: { params } }) => {
     <div>
       <Query query={LAUNCH_QUERY} variables={{ flight_number }}>
         {({ loading, error, data }) => {
-          console.log(data);
-          return <h4>Launch</h4>;
+          if (loading) return <h4>Loading...</h4>;
+          if (error) console.log(error);
+          const {
+            mission_name,
+            flight_number,
+            launch_year,
+            launch_success,
+            rocket: { rocket_id, rocket_name, rocket_type },
+          } = data.launch;
+          return (
+            <div>
+              <h1 className='display-4 my-3'>
+                <span className='text-dark'>Mission:</span> {mission_name}
+              </h1>
+              <h4 className='mb-3'>Launch Details</h4>
+              <ul className='list-group'>
+                <li className='list-group-item'>
+                  Flight Number: {flight_number}
+                </li>
+                <li className='list-group-item'>Launch Year: {launch_year}</li>
+                <li className='list-group-item'>
+                  Launch Successful:{' '}
+                  <span
+                    className={classNames({
+                      'text-success': launch_success,
+                      'text-danger': !launch_success,
+                    })}
+                  >
+                    {launch_success ? 'Yes' : 'No'}
+                  </span>
+                </li>
+              </ul>
+              <h4 className='my-3'>Rocket Details</h4>
+              <ul className='list-group'>
+                <li className='list-group-item'>Rocket ID: {rocket_id}</li>
+                <li className='list-group-item'>Rocket Name: {rocket_name}</li>
+                <li className='list-group-item'>Rocket Type: {rocket_type}</li>
+              </ul>
+              <hr></hr>
+              <Link to='/' className='btn btn-secondary'>
+                Back
+              </Link>
+            </div>
+          );
         }}
       </Query>
     </div>
   );
 };
 
-export default withRouter(Launch);
+export default Launch;
